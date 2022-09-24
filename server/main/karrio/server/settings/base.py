2137d48cb7ee8d83dcd113c2ed898093116da9c9
Daniel K <danielk.developer@gmail.com>
2022-09-24 03:08:51 -0700

    (replace) drf-yasg by drf-spectacular for OpenAPI v3 support

--- server/main/karrio/server/settings/base.py
+++ server/main/karrio/server/settings/base.py
@@ -221,7 +221,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "django_email_verification",
     "rest_framework_tracking",
-    "drf_yasg",
+    "drf_spectacular",
     "constance.backends.database",
     "huey.contrib.djhuey",
     "corsheaders",
@@ -349,6 +349,7 @@ PERMISSION_CLASSES = [
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": PERMISSION_CLASSES,
     "DEFAULT_AUTHENTICATION_CLASSES": AUTHENTICATION_CLASSES,
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.JSONRenderer",
     ],
@@ -413,7 +414,11 @@ OAUTH2_PROVIDER = {
 }
 
 # OpenAPI config
-
+SPECTACULAR_SETTINGS = {
+    "VERSION": VERSION,
+    "SERVE_INCLUDE_SCHEMA": False,
+    "ENUM_NAME_OVERRIDES": {},
+}
 SWAGGER_SETTINGS = {
     "USE_SESSION_AUTH": False,
     "LOGIN_URL": reverse_lazy("admin:login"),
